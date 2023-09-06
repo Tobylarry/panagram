18707e3be53d414b08fa2db5f6314a1c6ed93660
Tobi <47112773+Tobylarry@users.noreply.github.com>
2023-09-05 22:59:38 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+//panagram
+
+/**
+
+A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+ */
+
+
+/**
+ * @param {string} sentence
+ * @return {boolean}
+ */
+var checkIfPangram = function(sentence) {
+    return new Set(sentence).size == 26;
+};
